8a07d49134bf4618585a9c7604255c6fe1f69e41
L1ppi <filippicgodoy@gmail.com>
2025-03-07 21:41:47 -0300

    Add files via upload

--- contador.py
+++ contador.py
@@ -6,47 +6,50 @@ import flet as ft
 def main(page: ft.Page):
     # 2º definir título para a página
     page.title = "Contador"
-    #definir tamanho da janela
+    # definir tamanho da janela
     page.window.width = 500
-    page.window.height = 900
+    page.window.height = 700
+    page.window.center()
     
     # definir o alinhamento dos componentes na página
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-    #page.horizontal_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.MainAxisAlignment.CENTER
     
     # definir a cor de fundo da página
-    page.bgcolor = "#A9B5DF"
+    page.bgcolor = "#FFF2F2"
     
     # definição da variável numero como textfield
     numero = ft.TextField(
-        #valor inicial no textfield
+        # valor inicial no textfield
         value="0",
-        #comprimento do textfield
+        # comprimento do textfield
         width=300,
-        #Posicionamento do textfield
+        # Posicionamento do textfield
         text_align=ft.TextAlign.CENTER,
-        #texto em negritos
+        # texto em negritos
         text_style=ft.TextStyle(weight=ft.FontWeight.BOLD),
-        #tamanho da fonte
-        text_size=30,
-        #texto para orientação 
+        # tamanho da fonte
+        text_size=16,
+        # texto para orientação 
         label="Quantidade",
-        label_style=ft.TextStyle(weight=ft.FontWeight.BOLD),
-        #cor do background
-        bgcolor=ft.colors.CYAN,
-        #bordas total ou interior
-        border=ft.InputBorder.OUTLINE,
-        #espessura da borda
+        label_style=ft.TextStyle(weight=ft.FontWeight.BOLD, color="#2D336B"),
+        # cor do background
+        bgcolor="#A9B5DF",
+        # bordas total ou interior
+        border=ft.InputBorder.UNDERLINE,
+        # espessura da borda
         border_width=1,
-        #cor da borda
-        border_color=ft.colors.BLUE,
-        #Borda com cantos arredendados
+        # cor da borda
+        border_color="#2D336B",
+        # Borda com cantos arredendados
         border_radius=15,
-        #cor do texto
-        color=ft.colors.YELLOW,
-        #filtro de entrada
+        # cor do texto
+        color="#2D336B",
+        # filtro de entrada
         input_filter=ft.NumbersOnlyInputFilter(),
-        #máximo de caracteres
+        # somente leitura
+        read_only=True,
+        # máximo de caracteres
         max_length=3,
     )
     
@@ -54,35 +57,54 @@ def main(page: ft.Page):
     def sub(e):
         if int(numero.value) > 0:
             numero.value = str(int(numero.value) - 1)
-            numero.error_text=None
+            numero.error_text = None
         else:
-            #texto para erros
-            numero.error_text="COMO VC QUER MENOS UM PRODUTO?"
+            # texto para erros
+            numero.error_text = "VALOR INVÁLIDO"
         page.update()
-    
+
     # FUNÇÃO PARA SOMAR 1 AO VALOR ATUAL
     def som(e):
-        if int(numero.value) <10:
+        if int(numero.value) < 5:
             numero.value = str(int(numero.value) + 1)
-            numero.error_text=None
+            numero.error_text = None
         else:
-            numero.error_text="SÓ VAI TA PODENDO DEZ MEU CRIA"
+            numero.error_text = "MÁXIMO 5"
         page.update()
     
     # 3º criar a área da página
     page.add(
+        ft.Container(
+            content=ft.Text(
+                value="---Contador de Itens---",
+                weight=ft.FontWeight.BOLD,
+                size=24,
+                color="#2D336B",
+                bgcolor="#A9B5DF",
+            ),
+            alignment=ft.alignment.top_center,
+        ),
+        ft.Container(
+            content=ft.Text(
+                value="Clique no + para adicionar e no - para subtrair a quantidade de itens",
+                weight=ft.FontWeight.NORMAL,
+                size=14,
+                color="#2D336B",
+            ),
+            alignment=ft.alignment.center,
+        ),
         # 4º criar uma linha para adicionar os elementos na página
         ft.Row(
             # 5º adicionar os elementos: (-) numero (+)
             controls=[
-                ft.IconButton(icon=ft.icons.REMOVE_CIRCLE_OUTLINE, on_click=sub),
+                ft.IconButton(icon=ft.icons.REMOVE_CIRCLE_OUTLINE, on_click=sub, icon_color="#2D336B"),
                 numero,
-                ft.IconButton(icon=ft.icons.ADD_CIRCLE_OUTLINE, on_click=som),
+                ft.IconButton(icon=ft.icons.ADD_CIRCLE_OUTLINE, on_click=som, icon_color="#2D336B"),
             ],
             alignment=ft.MainAxisAlignment.CENTER,
+            vertical_alignment=ft.CrossAxisAlignment.CENTER,
         )
     )
-    
     
     page.update()
 
